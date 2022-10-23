4c94d86807c3b337fcd45524ac454bb314bd252c
Paulo Renato <paulorenatorf@hotmail.com>
2022-10-23 19:25:58 -0300

    created handle submit function

--- src/components/LinkShortener/LinkShortener.tsx
+++ src/components/LinkShortener/LinkShortener.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LinkShortener = () => {
+  const [linkValue, setLinkValue] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (!/^[a-zA-Z]\.[a-zA-Z]$/.test(linkValue)) {
+      setError("please add a valid link");
+      return;
+    }
+  };
+
   return (
     <div className="">
+      {error !== "" && <p data-testid="error">{error}</p>}
       <input
         className="border border-darkViolet"
         type="text"
+        onChange={(e) => setLinkValue(e.target.value)}
         placeholder="Shorten a link here..."
         id="link-shortener"
       />
-      <button className="border border-darkViolet">Shorten it!</button>
+      <button
+        className="border border-darkViolet"
+        onClick={(e) => handleSubmit(e)}
+      >
+        Shorten it!
+      </button>
     </div>
   );
 };
