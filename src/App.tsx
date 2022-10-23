40bb4fd835a05b619506391dddddf332313a81e8
Paulo Renato <paulorenatorf@hotmail.com>
2022-10-23 19:15:16 -0300

    Link shortener page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import LinkShortener from "./components/LinkShortener/LinkShortener";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <div className="mx-8">
         <Header />
+        <LinkShortener />
       </div>
     </div>
   );
--- src/components/LinkShortener/LinkShortener.tsx
+++ src/components/LinkShortener/LinkShortener.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const LinkShortener = () => {
-  return <div>LinkShortener</div>;
+  return (
+    <div className="">
+      <input
+        className="border border-darkViolet"
+        type="text"
+        placeholder="Shorten a link here..."
+        id="link-shortener"
+      />
+      <button className="border border-darkViolet">Shorten it!</button>
+    </div>
+  );
 };
 
 export default LinkShortener;
